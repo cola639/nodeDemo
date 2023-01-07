5baf763cfed1f0eb758cc304a82bd918fa1f6b9b
cola639 <337507950@qq.com>
2023-01-07 13:12:27 +0800

    add

--- node-crud-template/index.js
+++ node-crud-template/index.js
@@ -1,5 +1,6 @@
 const winston = require('winston')
 const express = require('express')
+const config = require('config')
 const app = express()
 
 require('./startup/logging')()
@@ -8,8 +9,7 @@ require('./startup/db')()
 require('./startup/config')() // 引入自启动函数
 require('./startup/validation')()
 
-const port = process.env.PORT || 3000
+const port = config.get('port') || 3000
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`))
 
 module.exports = server
-s
--- node-crud-template/startup/routes.js
+++ node-crud-template/startup/routes.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors')
+const corsOptions = require('../config/corsOptions')
 const genres = require('../routes/genres')
 const customers = require('../routes/customers')
 const movies = require('../routes/movies')
@@ -10,6 +12,7 @@ const error = require('../middleware/error')
 
 module.exports = function (app) {
   app.use(express.json())
+  app.use(cors(corsOptions))
   app.use('/api/genres', genres)
   app.use('/api/customers', customers)
   app.use('/api/movies', movies)
